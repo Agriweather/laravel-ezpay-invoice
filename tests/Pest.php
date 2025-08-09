13395028232aa2fbdfa3df0e5d390bffb6cad4dc
Lucas Yang <yangchenshin77@gmail.com>
2025-08-09 20:38:51 +0800

    Support old version dependencies

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,3 +1,8 @@
 <?php
 
-pest()->extend(Agriweather\EzPayInvoice\Tests\TestCase::class)->in('Feature');
+if (function_exists('pest')) {
+    pest()->extend(Agriweather\EzPayInvoice\Tests\TestCase::class)->in('Feature');
+} else {
+    // Fallback for Pest v1.x
+    uses(Agriweather\EzPayInvoice\Tests\TestCase::class)->in('Feature');
+}
