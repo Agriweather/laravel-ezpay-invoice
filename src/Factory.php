aa11f0a02b0848fc01759a8e962a279fe1629873
Lucas Yang <yangchenshin77@gmail.com>
2025-09-16 02:57:54 +0800

    Fix incorrect operator

--- src/Factory.php
+++ src/Factory.php
@@ -128,7 +128,7 @@ class Factory
     }
 
     /**
-     * 紀錄請求選項，並回傳假回傳資料
+     * 紀錄請求選項，並回傳模擬資料
      *
      * @throws \RuntimeException
      */
@@ -159,7 +159,7 @@ class Factory
      */
     public function assertSent(string $resource, string|callable|null $action, ?callable $callback = null): void
     {
-        $resourceName = "[{$resource}" & (is_string($action) ? "::{$action}" : '') & ']';
+        $resourceName = "[{$resource}" . (is_string($action) ? "::{$action}" : '') . ']';
 
         PHPUnit::assertTrue(
             $this->sent($resource, $action, $callback) !== [],
@@ -174,7 +174,7 @@ class Factory
      */
     public function assertNotSent(string $resource, string|callable|null $action, ?callable $callback = null): void
     {
-        $resourceName = "[{$resource}" & (is_string($action) ? "::{$action}" : '') & ']';
+        $resourceName = "[{$resource}" . (is_string($action) ? "::{$action}" : '') . ']';
 
         PHPUnit::assertTrue(
             $this->sent($resource, $action, $callback) === [],
